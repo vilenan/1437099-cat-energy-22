495dd67ca0b6be2dfccd697b474c23215fc3293c
Vilenan <vilenan@mail.ru>
2021-05-21 20:41:14 +0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -148,7 +148,7 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
